b03c60902b11044e5b3c20632fa2fe2ec3976934
holtzmano <holtzmanoren99@gmail.com>
2024-03-06 17:32:19 +0200

    added bootstrap

--- src/Series.js
+++ src/Series.js
@@ -6,27 +6,31 @@ function Series({ idPrefix, team1Id, team2Id, onScoreChange }) {
         onScoreChange(idPrefix, team, score);
     };
     return (
-        <div className="series">
-            <label htmlFor={`${idPrefix}Team1`}>{team1Id}:</label>
-            <input
-                type="number"
-                id={`${idPrefix}Team1`}
-                name={`${idPrefix}Team1`}
-                min="0"
-                max="4"
-                required
-                onChange={handleInputChange.bind(null, 'team1')}
-            />
-            <label htmlFor={`${idPrefix}Team2`}>{team2Id}:</label>
-            <input
-                type="number"
-                id={`${idPrefix}Team2`}
-                name={`${idPrefix}Team2`}
-                min="0"
-                max="4"
-                required
-                onChange={handleInputChange.bind(null, 'team2')}
-            />
+        <div className="series mb-3">
+            <div className="input-group">
+                <span className='input-group-text'>{team1Id.toUpperCase()}</span>
+                <input
+                    type='number'
+                    className='form-control'
+                    id={`${idPrefix}-team1`}
+                    name={`${idPrefix}-team1`}
+                    min='0'
+                    max='4'
+                    required
+                    onChange={handleInputChange.bind(null, 'team1')}
+                />
+                <span className='input-group-text'>{team2Id.toUpperCase()}</span>
+                <input
+                    type='number'
+                    className='form-control'
+                    id={`${idPrefix}-team2`}
+                    name={`${idPrefix}-team2`}
+                    min='0'
+                    max='4'
+                    required
+                    onChange={handleInputChange.bind(null, 'team2')}
+                />
+            </div>
         </div>
     );
 }
